238fa7d8544e3f5fdc45c99dc75343ed89f89ffa
Naman Agrahari <agrahari.nam@gmail.com>
2019-01-25 18:42:42 +0530

    separate footer page

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer class="page-footer white"><center>
+    <p class="menuheading animated">CONTACT</p>
+    <p class="text" style="padding: 0px; font-weight: bold"> Office of Alumni Affairs and International Relations</p>
+    <p class="smalltext" style="margin-top: -20px;">alumni@hijli.iitkgp.ernet.in<br>
+    03222 281860</p>
+    <div class="container">
+      <div class="row">
+        <p class="text" style="padding: 0px; margin-top: -20px; font-weight: bold">Students' Alumni Cell General Secretaries</p>
+        <div class="col l4 m4 s12 offset-l2 offset-m2">
+          <p class="smalltext" style="margin-top: -20px;">Vasanta Majety<br>
+            vasantamajety@gmail.com<br>
+            +91 7477745444
+          </p>
+        </div>
+        <div class="col l4 m4 s12 ">
+          <p class="smalltext" style="margin-top: -20px;">Gautam Arya<br>
+            gautam25arya@gmail.com<br>
+          +91 8349702291</p>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <p class="text black" style="padding-top: 5px; margin-top: 0px; padding-bottom: 0px; margin-bottom: 0px;height: 50px">&copy;Copyrights reserved 2018</p>
+    </div>
+  </center>
+</footer>
